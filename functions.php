0b3d834b578aea5782fb30f94fdeacf2f41151bd
Pratik-Tambekar <41792468+Pratik-Tambekar@users.noreply.github.com>
2018-07-27 19:02:02 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+spl_autoload_register(function($className){
+
+	$path=strtolower($className) .".php";
+	if(file_exists($path)){
+		require_once($path);
+	}else
+	{
+		echo "file $path is not found.";
+	}
+
+
+})
+
+
+?>
